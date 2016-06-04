ad73cd312b1c5cae2d7f1f5682c0ef1381399081
Tannin <sherb@gmx.net>
2016-06-04 20:26:05 +0200

    translation

---
+++ src/check_fnis_en.ts
@@ -0,0 +1,53 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>CheckFNIS</name>
+    <message>
+        <location filename="checkfnis.cpp" line="64"/>
+        <source>Checks if FNIS behaviours need to be updated whenever you start the game. This is only relevant for Skyrim and if FNIS is installed.&lt;br&gt;&lt;i&gt;You will need to restart MO after you enable/disable this plugin for the change to take effect.&lt;/i&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="159"/>
+        <source>Run FNIS before %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="160"/>
+        <source>FNIS source data has been changed. You should run GenerateFNIS.exe now.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="166"/>
+        <source>Failed to start %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="172"/>
+        <location filename="checkfnis.cpp" line="179"/>
+        <source>Start %1?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="173"/>
+        <source>FNIS reported a %1, do you want to run the application anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="174"/>
+        <source>warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="175"/>
+        <source>critical error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="180"/>
+        <source>Failed to determine fnis exit code, do you want to run the application anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
