35d4734e1e6935e2b85e2b88ceb14fb75fbd32ed
Silarn <jrim@rimpo.org>
2018-04-10 23:28:34 -0500

    Prep for Qt 5.11 (backward-compatible)

--- src/check_fnis_en.ts
+++ src/check_fnis_en.ts
@@ -4,49 +4,57 @@
 <context>
     <name>CheckFNIS</name>
     <message>
-        <location filename="checkfnis.cpp" line="64"/>
-        <source>Checks if FNIS behaviours need to be updated whenever you start the game. This is only relevant for Skyrim and if FNIS is installed.&lt;br&gt;&lt;i&gt;You will need to restart MO after you enable/disable this plugin for the change to take effect.&lt;/i&gt;</source>
+        <location filename="checkfnis.cpp" line="79"/>
+        <source>Checks if FNIS behaviours need to be updated whenever you start the game. This is only relevant for Skyrim and if FNIS is installed.&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="159"/>
+        <location filename="checkfnis.cpp" line="211"/>
         <source>Run FNIS before %1?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="160"/>
+        <location filename="checkfnis.cpp" line="212"/>
         <source>FNIS source data has been changed. You should run GenerateFNIS.exe now.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="166"/>
+        <location filename="checkfnis.cpp" line="219"/>
         <source>Failed to start %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="172"/>
-        <location filename="checkfnis.cpp" line="179"/>
+        <location filename="checkfnis.cpp" line="225"/>
+        <location filename="checkfnis.cpp" line="232"/>
+        <location filename="checkfnis.cpp" line="256"/>
         <source>Start %1?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="173"/>
+        <location filename="checkfnis.cpp" line="226"/>
         <source>FNIS reported a %1, do you want to run the application anyway?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="174"/>
+        <location filename="checkfnis.cpp" line="227"/>
+        <location filename="checkfnis.cpp" line="258"/>
         <source>warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="175"/>
+        <location filename="checkfnis.cpp" line="228"/>
+        <location filename="checkfnis.cpp" line="259"/>
         <source>critical error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="180"/>
-        <source>Failed to determine fnis exit code, do you want to run the application anyway?</source>
+        <location filename="checkfnis.cpp" line="233"/>
+        <source>Failed to determine FNIS exit code, do you want to run the application anyway?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="257"/>
+        <source>FNIS reported a %1. Do you want to assume it worked?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
