e45f3e3de6cd6e73a7707ac2d73768adaf37943c
Mikaël Capelle <capelle.mikael@gmail.com>
2021-02-02 20:39:12 +0100

    Update translation file.

--- src/check_fnis_en.ts
+++ src/check_fnis_en.ts
@@ -4,56 +4,61 @@
 <context>
     <name>CheckFNIS</name>
     <message>
-        <location filename="checkfnis.cpp" line="79"/>
+        <location filename="checkfnis.cpp" line="74"/>
+        <source>FNIS Checker</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="checkfnis.cpp" line="84"/>
         <source>Checks if FNIS behaviours need to be updated whenever you start the game. This is only relevant for Skyrim and if FNIS is installed.&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="211"/>
+        <location filename="checkfnis.cpp" line="216"/>
         <source>Run FNIS before %1?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="212"/>
+        <location filename="checkfnis.cpp" line="217"/>
         <source>FNIS source data has been changed. You should run GenerateFNIS.exe now.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="219"/>
+        <location filename="checkfnis.cpp" line="224"/>
         <source>Failed to start %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="225"/>
-        <location filename="checkfnis.cpp" line="232"/>
-        <location filename="checkfnis.cpp" line="256"/>
+        <location filename="checkfnis.cpp" line="230"/>
+        <location filename="checkfnis.cpp" line="237"/>
+        <location filename="checkfnis.cpp" line="261"/>
         <source>Start %1?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="226"/>
+        <location filename="checkfnis.cpp" line="231"/>
         <source>FNIS reported a %1, do you want to run the application anyway?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="227"/>
-        <location filename="checkfnis.cpp" line="258"/>
+        <location filename="checkfnis.cpp" line="232"/>
+        <location filename="checkfnis.cpp" line="263"/>
         <source>warning</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="228"/>
-        <location filename="checkfnis.cpp" line="259"/>
+        <location filename="checkfnis.cpp" line="233"/>
+        <location filename="checkfnis.cpp" line="264"/>
         <source>critical error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="233"/>
+        <location filename="checkfnis.cpp" line="238"/>
         <source>Failed to determine FNIS exit code, do you want to run the application anyway?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="checkfnis.cpp" line="257"/>
+        <location filename="checkfnis.cpp" line="262"/>
         <source>FNIS reported a %1. Do you want to assume it worked?</source>
         <translation type="unfinished"></translation>
     </message>
