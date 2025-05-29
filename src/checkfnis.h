a8153b15090956a662fe4f3e5d05b5a64ced8112
Mikaël Capelle <capelle.mikael@gmail.com>
2025-05-29 11:18:11 +0200

    Add precommit and fix formatting.

--- src/checkfnis.h
+++ src/checkfnis.h
@@ -1,8 +1,8 @@
 #ifndef CHECKFNIS_H
 #define CHECKFNIS_H
 
-#include <uibase/iplugin.h>
 #include <uibase/imoinfo.h>
+#include <uibase/iplugin.h>
 #include <uibase/versioninfo.h>
 
 #include <QList>
@@ -10,13 +10,13 @@
 #include <QObject>
 #include <QString>
 
-#include <QtGlobal> // for QT_VERSION, QT_VERSION_CHECK
+#include <QtGlobal>  // for QT_VERSION, QT_VERSION_CHECK
 
 #include <vector>
 
 namespace MOBase
 {
-  struct PluginSetting;
+struct PluginSetting;
 }
 
 class CheckFNIS : public QObject, public MOBase::IPlugin
@@ -32,34 +32,36 @@ public:
   CheckFNIS();
   ~CheckFNIS();
 
-public: // IPlugin
-  virtual bool init(MOBase::IOrganizer *moInfo) override;
+public:  // IPlugin
+  virtual bool init(MOBase::IOrganizer* moInfo) override;
   virtual QString name() const override;
   virtual QString localizedName() const override;
-  virtual std::vector<std::shared_ptr<const MOBase::IPluginRequirement>> requirements() const override;
+  virtual std::vector<std::shared_ptr<const MOBase::IPluginRequirement>>
+  requirements() const override;
   virtual QString author() const override;
   virtual QString description() const override;
   virtual MOBase::VersionInfo version() const override;
   virtual QList<MOBase::PluginSetting> settings() const override;
 
 private:
-  bool fnisCheck(const QString &application);
-  void fnisEndCheck(const QString &application, unsigned int code);
+  bool fnisCheck(const QString& application);
+  void fnisEndCheck(const QString& application, unsigned int code);
 
   QString getFnisPath() const;
-  static bool appIsFNIS(QString const &application, const QString &fnisApp);
+  static bool appIsFNIS(QString const& application, const QString& fnisApp);
 
-  bool testFileRelevant(const MOBase::IOrganizer::FileInfo &fileName) const;
-  void findRelevantFilesRecursive(const QString &path, QMap<QString, QString> &fileList) const;
+  bool testFileRelevant(const MOBase::IOrganizer::FileInfo& fileName) const;
+  void findRelevantFilesRecursive(const QString& path,
+                                  QMap<QString, QString>& fileList) const;
   // generates a map of file-names and hashes. Any change prompts a new fnis run
   QString generateIdentifier() const;
 
 private:
-  MOBase::IOrganizer *m_MOInfo;
+  MOBase::IOrganizer* m_MOInfo;
   bool m_Active;
 
   std::vector<QRegularExpression> const m_MatchExpressions;
   std::vector<QRegularExpression> const m_SensitiveMatchExpressions;
 };
 
-#endif // CHECKFNIS_H
+#endif  // CHECKFNIS_H
